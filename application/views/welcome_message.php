043f1a48b8c6e6d5b9a3b008ab78ea6801709b39
Kgotso Maduna <kgmaduna01@gmail.com>
2021-07-02 10:03:20 +0200

    my Portfolio

---
+++ application/views/welcome_message.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	
+
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="profile">
+  <img src="Assets/images/kgotso.jpg" alt="" style="width:50%">
+  <h1>Kgotso Maduna</h1>
+  <p class="title">Software Development Intern</p>
+  <p>Tshwane University of Technology</p>
+  <a href="http://localhost/myPortfolio/"><i class="fa fa-dribbble"></i></a>
+  <a href="https://www.linkedin.com/in/kgotso-maduna-24a4641aa/"><i class="fa fa-linkedin"></i></a>
+  
+  <p><button>Contact</button></p>
+</div>
+
+</head>
+<body>
+	
+</body>
+</html>
